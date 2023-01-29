7a4601b2f98c30161a2aa94df5f2c7bc7d270789
Tomohiko Ozawa <kota65535@gmail.com>
2023-01-29 16:10:14 +0900

    test

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -20,3 +20,46 @@ provider "aws" {
 module "rds_saver" {
   source = "../../"
 }
+
+resource "aws_rds_cluster" "main" {
+  cluster_identifier   = "test"
+  engine               = "aurora-mysql"
+  engine_version       = "8.0.mysql_aurora.3.02.0"
+  engine_mode          = "provisioned"
+  master_username      = "root"
+  db_subnet_group_name = aws_db_subnet_group.main.name
+
+  serverlessv2_scaling_configuration {
+    min_capacity = 1
+    max_capacity = 2
+  }
+  apply_immediately = true
+  tags = {
+    AutoStartTime = 10
+    AutoStopTime  = 11
+  }
+}
+
+resource "aws_db_instance" "main" {
+  identifier           = "test2"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  allocated_storage    = 5
+  username             = "root"
+  password             = "rootroot"
+  db_subnet_group_name = aws_db_subnet_group.main.name
+  apply_immediately    = true
+  tags = {
+    AutoStartTime = 10
+    AutoStopTime  = 11
+  }
+}
+
+resource "aws_db_subnet_group" "main" {
+  name       = "test"
+  subnet_ids = ["subnet-0abaada26acb8894f", "subnet-0ef9f8fc78d0642d8"]
+  tags = {
+    Name = "test"
+  }
+}
